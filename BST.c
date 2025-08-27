ed1906fd05b706cd52beddbce17f47dceea0111f
MaddoxHodges <117109277+MaddoxHodges@users.noreply.github.com>
2025-08-27 16:16:14 -0500

    Update BST.c

--- BST.c
+++ BST.c
@@ -2,16 +2,16 @@
 #include <stlib.h>
 #include <string.h>
 
-
-int insertR(struct Node **node, int y)
 {
   if (*node == NULL)
   {
     *node = makeNode(y);
     return 1;
    }
-  if ((*node)->val<y) return insertR(&((*node)->left), y);
+  if ((*node)->val<y) return insertBST(&((*node)->left), y);
 }
+
+int SearchBST(struct Node 
 int main() 
 {
   
