5c626fc7a280715b9dfbf8042af52c13c364ab7c
MaddoxHodges <117109277+MaddoxHodges@users.noreply.github.com>
2025-08-27 16:07:17 -0500

    Update BST.c

--- BST.c
+++ BST.c
@@ -3,8 +3,18 @@
 #include <string.h>
 
 
+int insertR(struct Node **node, int y)
+{
+  if (*node == NULL)
+  {
+    *node = makeNode(y);
+    return 1;
+   }
+  if ((*node)->val<y) return insertR(&((*node)->left), y);
+}
 int main() 
 {
+  
 
   
 
